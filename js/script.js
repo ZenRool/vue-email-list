0241b5b930d33a7efdf784ddfc1c91c7da8e3bb0
Zenrool <lorenzopapperini@hotmail.it>
2022-05-13 17:09:02 +0200

    genera 10 email e le pusha nel array, con Vue scrive l'array nel dom, non aspetta che vengano generate tutte le email

--- js/script.js
+++ js/script.js
@@ -3,9 +3,20 @@
 const app = new Vue( {
     el: '#root',
     data: {
+        mails: []
      
     },
+    created() {
+        this.generateMails();
+    },
     methods: {
+        generateMails() {
+            for(let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
+                    this.mails.push(resp.data.response);
+                });
+            }
+        }
 
     }
 }
