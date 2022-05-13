9141b10259a0969e527502554d8e1bca65a040d3
Zenrool <lorenzopapperini@hotmail.it>
2022-05-13 17:30:02 +0200

    Scrive l'emails nel dom solo quando sono 10

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const app = new Vue( {
     el: '#root',
     data: {
         mails: []
-     
     },
     created() {
         this.generateMails();
@@ -13,11 +12,10 @@ const app = new Vue( {
         generateMails() {
             for(let i = 0; i < 10; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
-                    this.mails.push(resp.data.response);
+                    this.mails.push(resp.data.response); 
                 });
             }
         }
-
     }
 }
 );
